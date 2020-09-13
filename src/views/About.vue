4ed67a49d03b89013d9b1d9172ed5951bcf7d12f
leolecrado <leocrapart@yahoo.fr>
2020-09-13 18:35:55 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Sublime text is here!</h1>
+    <h2>And here too</h2>
+    <div class="text-xl font-semibold underline">Tailwind css touch</div>
+    <h3>Commit to git !</h3>
   </div>
 </template>
